6bf6e41616c715afb8d9ae207c83f355dd9811af
Mahima Bhat <mahima.2428cseaiml51@kiet.edu>
2026-04-22 23:48:27 +0530

    fix build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
-import Register from "./pages/Register";
+
 function App() {
   return (
     <BrowserRouter>
